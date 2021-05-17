78aea7bd139a973b9621db49a921f6280457ae5f
NASEELA K T <naslalellu@gmail.com>
2021-05-17 13:46:35 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 
 function App() {
-  const jsxElement = <h1>I am a JSX element</h1>
-  const welcome = <h1>Welcome to  React Workshop</h1>
-  const data = <small>By Skill Safari</small>
-  return (
-  <div className="App">
-    {jsxElement}
-    {welcome}
-    {data}
-  </div>
+  const header = (
+    <header>
+      <h1>Welcome to React Workshop</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+    </header>
   );
+return <div className="App">{header}</div>;
 }
 
 export default App;
