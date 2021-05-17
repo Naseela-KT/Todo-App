5e24e5c65a41928367527c1745d96f7695f465e4
NASEELA K T <naslalellu@gmail.com>
2021-05-17 13:38:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 
 function App() {
+  const jsxElement = <h1>I am a JSX element</h1>
+  const welcome = <h1>Welcome to  React Workshop</h1>
+  const data = <small>By Skill Safari</small>
   return (
-    <div className="App"></div>
+  <div className="App">
+    {jsxElement}
+    {welcome}
+    {data}
+  </div>
   );
 }
 
