209264efb692bfd44635cb4a8d0b2c1591c94e5f
NASEELA K T <naslalellu@gmail.com>
2021-05-17 17:50:01 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
 return <div className="App">
   <input type="text" placeholder="Enter Your Name"></input>
+  <Button/>
 </div>;
 }
 
