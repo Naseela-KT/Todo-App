850461d13c88dc9ff9ed965fe2636dcbfbf13b03
NASEELA K T <naslalellu@gmail.com>
2021-05-18 19:38:00 +0530

    c

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
 return (<div className="App">
   <input type="text" placeholder="Enter Your Name"></input>
   <Button BtnName="Click Here"/>
-  <Button BtnName="Sign in"/>
-  <Button BtnName="Login"/>
-  <Button BtnName="Logout"/>
 </div>);
 }
 export default App;
