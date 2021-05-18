c0954986271c6880589652a0057ebbbbd99ba6ad
NASEELA K T <naslalellu@gmail.com>
2021-05-18 20:43:11 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Button from './components/Button';
 
 function App() {
 return (<div className="App">
   <input type="text" placeholder="Enter Your Name"></input>
-  <Button BtnName="Click Here"/>
 </div>);
 }
 export default App;
