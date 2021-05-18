c5e9d3b5710b4e45638ed89d236cded4b17b4c85
NASEELA K T <naslalellu@gmail.com>
2021-05-18 20:47:14 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
 function App() {
-return (<div className="App">
-  <input type="text" placeholder="Enter Your Name"></input>
-</div>);
+return (
+  <div className="App">
+   <div className="container">
+    <h1>Todo List Application</h1>
+   </div>
+  </div>
+  );
 }
 export default App;
