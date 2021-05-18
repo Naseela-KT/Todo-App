5b2e0bf7ea4d66701b72d88a357bf5d2f7dc9a96
NASEELA K T <naslalellu@gmail.com>
2021-05-18 21:11:31 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import TodoForm from './components/TodoForm';
 function App() {
 return (
   <div className="App">
    <div className="container">
     <h1>Todo List Application</h1>
+    <TodoForm />
    </div>
   </div>
   );
---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoForm = () => {
+    return (
+        <form className='formControl'>
+            <input type="text" placeholder="Enter Tour Todo"></input>
+        </form>
+    );
+}
+
+export default TodoForm
