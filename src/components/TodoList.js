02915f15a872d8c57507abca70cb23549002df3c
NASEELA K T <naslalellu@gmail.com>
2021-05-21 22:07:06 +0530

    c

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TodoList = ({todos,deleteHandler}) => {
+const TodoList = ({ todos , deleteHandler }) => {
     return (
         <ul className="listControl">
             {
